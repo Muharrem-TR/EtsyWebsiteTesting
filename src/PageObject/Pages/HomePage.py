0141a97edbabd9fb0ac830671aa5939dedb573f6
Muharrem <m.karapazar2@gmail.com>
2022-12-09 01:46:45 +0300

    HomePage Update

--- src/PageObject/Pages/HomePage.py
+++ src/PageObject/Pages/HomePage.py
@@ -1,19 +1,25 @@
+import random
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.select import Select
 
 base_url = "https://www.etsy.com/"
 
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
-        self.acceptButton_CSS = "button[data-uia='cookie-disclosure-button-accept']"
+        self.siteSettingsBtn_ID = "locale-picker-trigger"
+        self.saveBtn_Name = "save"
 
 
 
-    def get_acceptButton_CSS(self):
-        return self.driver.find_element(By.CSS_SELECTOR, self.acceptButton_CSS)
+
+    def get_siteSettingsBtn_ID(self):
+        return self.driver.find_element(By.ID, self.siteSettingsBtn_ID)
+    def get_saveBtn_Name(self):
+        return self.driver.find_element(By.NAME, self.saveBtn_Name)
 
 
 
@@ -41,11 +47,46 @@ class HomePage:
         print("Bulunduğunuz sitenin ismi " + baslik.split("-")[0] + "URL adresi ise " + siteUrl + " olarak doğrulanmıştır.")
         print("<------------------------------------------------------------------------------------------------>")
 
+    def mainInformationOfThePage(self):
+        siteUrl = self.driver.current_url
+        self.findAndClick("siteSettingsBtn_ID")
+        region = self.driver.find_element(By.XPATH,"(//option[@selected='selected'])[1]").text
+        language = self.driver.find_element(By.XPATH,"(//option[@selected='selected'])[2]").text
+        # currency = self.driver.find_element(By.XPATH,"(//option[@selected='selected'])[3]").text.split(" ")[3]
+        # currency = self.driver.find_element(By.XPATH,"(//option[@selected='selected'])[3]").text[2:]
+        currency = self.driver.find_element(By.XPATH,"(//option[@selected='selected'])[3]").text[-4:-1]
+        print("<------------------------------------------------------------------>")
+        print("Site Ayarları= Bölge: "+region+" | Dil: "+language+" | Para Birimi: "+currency)
+        print("<------------------------------------------------------------------>")
+        self.driver.get(siteUrl)
+
+    def changeMainInformationOfRandomPage(self):
+        siteUrl = self.driver.current_url
+        self.findAndClick("siteSettingsBtn_ID")
+        ddmenuRegion = self.driver.find_element(By.ID, "locale-overlay-select-region_code")
+        regionMenusu = Select(ddmenuRegion)
+        randomRegionInt = random.randrange(1, len(regionMenusu.options))
+        regionMenusu.select_by_index(randomRegionInt)
+
+        ddmenuLanguage = self.driver.find_element(By.ID, "locale-overlay-select-language_code")
+        languageMenusu = Select(ddmenuLanguage)
+        randomLanguageInt = random.randrange(1, len(languageMenusu.options))
+        languageMenusu.select_by_index(randomLanguageInt)
+
+        ddmenuCurrency = self.driver.find_element(By.ID, "locale-overlay-select-currency_code")
+        currencyMenusu = Select(ddmenuCurrency)
+        randomCurrencyInt = random.randrange(1, len(currencyMenusu.options))
+        currencyMenusu.select_by_index(randomCurrencyInt)
+
+        self.findAndClick("saveBtn_Name")
+        self.driver.get(siteUrl)
+
 
 
     def findAndClick(self, strElement):
         match strElement:
-            case "acceptButton_CSS": self.myElement = self.get_acceptButton_CSS()
+            case "siteSettingsBtn_ID": self.myElement = self.get_siteSettingsBtn_ID()
+            case "saveBtn_Name": self.myElement = self.get_saveBtn_Name()
         self.scrollToElementMid(self.myElement)
         EC.element_to_be_clickable(self.myElement)
         self.myElement.click()
@@ -59,7 +100,7 @@ class HomePage:
 
     def findAndVerify(self, strElement, textElement):
         match strElement:
-            case "heroTitle_CSS": self.myElement = self.get_heroTitle_CSS()
+            case "siteSettingsMsg_CSS": self.myElement = self.get_siteSettingsMsg_CSS()
         self.scrollToElementMid(self.myElement)
         EC.visibility_of(self.myElement)
         assert textElement in self.myElement.text
@@ -68,3 +109,4 @@ class HomePage:
     @staticmethod
     def get_base_url():
         return base_url
+
